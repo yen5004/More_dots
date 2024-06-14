fb2de02d0ecfcce55640d6eb65241a8ce23dede3
yen5004 <119946331+yen5004@users.noreply.github.com>
2024-06-14 07:48:26 -0400

    Create add_dots.sh

---
+++ add_dots.sh
@@ -0,0 +1,18 @@
+#
+
+# Change dirs
+alias ..="cd .."
+alias cd..="cd .."
+alias ...="cd ../../"
+alias ....="cd ../../../"
+
+# CD and ls
+alias cdls="cd .. && ls"
+alias cdla="cd .. && ls -la"
+alias cdlh="cd .. && ls -lh"
+
+# 4. More ls aliases
+# Convenient shorthand for listing files, showing hidden files, and listing files in descending order of modification together with human readable file size.
+alias ll='ls -l'
+alias la='ls -Al'
+alias lt='ls -ltrh'
